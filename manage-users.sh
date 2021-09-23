fc2e02d34ea506e5f9800e1ee44c6a94cf8cd354
Paiet <78005127+Paiet@users.noreply.github.com>
2021-09-22 21:35:43 -0400

    Create manage-users.sh

---
+++ manage-users.sh
@@ -0,0 +1,80 @@
+#!/bin/bash
+
+# Description: Script to add and delete VPN peers from WG config
+
+
+while getopts 'hdau:' OPTION ; do
+
+	case "$OPTION" in
+
+
+		d) u_del=${OPTION}
+		;;
+		a) u_add=${OPTION}
+		;;
+		u) t_user=${OPTARG}
+		;;
+		h)
+
+			echo ""
+			echo "Usage: $(basename $0) [-a] | [-d] -u username"
+			echo ""
+			exit 1
+
+		;;
+		*)
+
+			echo "Invalid value."
+			ecit 1
+
+		;;
+
+	esac
+
+done
+
+
+# Check to see if the -a and -d are empty or if they are  both specified send error
+
+if [[ (${u_del} == "" && ${u_add} == "") || (${u_del} != "" && ${u_add} != "")  ]]
+
+then
+
+	echo "Please specify -a or -d and the -u and username."
+
+fi
+
+
+# Check to ensure -u is specified
+
+
+if [[ (${u_del} != "" ||  ${u_add} != "") && ${t_user} == ""  ]]
+
+then
+
+     echo "Please specify a user (-u)!"
+     echo "Usage:	$(basename $0) [-a] [-d] [-u username]"
+     exit 1
+
+fi
+
+# Delete a user
+
+if [[ ${u_del} ]]
+then
+
+	echo "Deleting ${t_user} from config file"
+	sed -i "/# ${t_user} begin/,/# ${t_user} end/d" wg0.conf
+	
+
+fi
+
+
+# Add a user
+if [[ ${u_add} ]]
+then
+
+	echo "Create the User"
+	bash peer.bash ${t_user}
+
+fi
